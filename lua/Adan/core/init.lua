b26ce928282eefe0dcb6fda5a3309a611eab3e3b
AdanWodzinski <adan.wodzinski@milwaukeetool.com>
2025-12-08 08:33:54 -0600

    add windows specifics to avoid looking into protected paths

--- lua/Adan/core/init.lua
+++ lua/Adan/core/init.lua
@@ -1,3 +1,4 @@
 require("Adan.core.options")
 require("Adan.core.keymaps")
 require("Adan.core.diagnostics")
+require("Adan.core.windows")
---
+++ lua/Adan/core/windows.lua
@@ -0,0 +1,11 @@
+local ok, wf = pcall(require, "vim.lsp._watchfiles")
+if ok then
+  -- Disable watching for Windows Defender directories
+  local original_create = wf._create
+  wf._create = function(path, opts, callback)
+    if path:match("Windows Defender") or path:match("ProgramData/Microsoft") then
+      return { cancel = function() end }
+    end
+    return original_create(path, opts, callback)
+  end
+end
