80e1c8c9d09ac36f6454170a42a913910f3a6d4c
AdanWodzinski <adan.wodzinski@milwaukeetool.com>
2025-12-10 22:04:42 -0600

    update nvim keymaps and formatting

--- lua/Adan/core/keymaps.lua
+++ lua/Adan/core/keymaps.lua
@@ -3,57 +3,49 @@
 -- =============================================================================
 vim.keymap.set("n", "<space>", "<Nop>")
 vim.g.mapleader = " "
-vim.g.maplocalleader = ' '
-
+vim.g.maplocalleader = " "
 
 -- =============================================================================
 -- BASIC EDITOR BEHAVIOR
 -- =============================================================================
 
 -- Clear search highlights
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Redo with capital U
 vim.keymap.set("n", "U", "<C-r>", { silent = true, noremap = true, desc = "Redo" })
 
 -- Q repeats last macro instead of entering ex mode
-vim.keymap.set('n', 'Q', '@@', { noremap = true, silent = true })
-
+vim.keymap.set("n", "Q", "@@", { noremap = true, silent = true })
 
 -- =============================================================================
 -- MOVEMENT & NAVIGATION
 -- =============================================================================
 
 -- Line navigation
-vim.keymap.set({ "n", "v", "o", "x" }, "gh", "0", { noremap = true, silent = true, desc = "go to begining of line" })
+vim.keymap.set({ "n", "v", "o", "x" }, "gh", "0", { silent = true, desc = "go to begining of line" })
 vim.keymap.set({ "n", "v", "o", "x" }, "gs", "^", {
-    noremap = true,
-    silent = true,
-    desc = "go to first not white space in line"
+	silent = true,
+	desc = "go to first not white space in line",
 })
 vim.keymap.set({ "n", "v", "o", "x" }, "gl", "$", {
-    noremap = true,
-    silent = true,
-    desc = "go to end of line"
+	silent = true,
+	desc = "go to end of line",
 })
 vim.keymap.set({ "n", "v", "o", "x" }, "ge", "G", {
-    noremap = true,
-    silent = true,
-    desc = "go to end of file"
+	silent = true,
+	desc = "go to end of file",
 })
 
 -- Page navigation (centered)
 vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", {
-    noremap = true,
-    silent = true,
-    desc = "jump up the page"
+	silent = true,
+	desc = "jump up the page",
 })
 vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", {
-    noremap = true,
-    silent = true,
-    desc = "jump down the page"
+	silent = true,
+	desc = "jump down the page",
 })
-
 
 -- =============================================================================
 -- SEARCH
@@ -61,62 +53,53 @@ vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", {
 
 -- Search within visual selection
 vim.keymap.set("v", "/", function()
-    vim.cmd('normal! "vy')
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>/\\%V', true, false, true), 'n', false)
+	vim.cmd('normal! "vy')
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>/\\%V", true, false, true), "n", false)
 end, { desc = "Search in visual selection" })
-
 
 -- =============================================================================
 -- COPY, PASTE & CLIPBOARD
 -- =============================================================================
 
 -- Internal (unnamed) register operations
-vim.keymap.set({ "n", "v" }, "y", "y", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "Y", "Y", { noremap = true, silent = true })
-vim.keymap.set("n", "p", "p", { noremap = true, silent = true, desc = "paste nvim clipboard at cursor" })
-vim.keymap.set("n", "P", "P", { noremap = true, silent = true, desc = "paste nvim clipboard before cursor" })
+vim.keymap.set({ "n", "v" }, "y", "y", { silent = true })
+vim.keymap.set({ "n", "v" }, "Y", "Y", { silent = true })
+vim.keymap.set("n", "p", "p", { silent = true, desc = "paste nvim clipboard at cursor" })
+vim.keymap.set("n", "P", "P", { silent = true, desc = "paste nvim clipboard before cursor" })
 
 -- System clipboard operations (Leader prefix)
 vim.keymap.set({ "n", "v" }, "<Leader>y", [["+y]], {
-    noremap = true,
-    silent = true,
-    desc = "save text to system clipboard"
+	silent = true,
+	desc = "save text to system clipboard",
 })
 vim.keymap.set("n", "<Leader>Y", [["+Y]], {
-    noremap = true,
-    silent = true,
-    desc = "save current line to system clipboard"
+	silent = true,
+	desc = "save current line to system clipboard",
 })
 vim.keymap.set("n", "<Leader>pp", [["+p]], {
-    noremap = true,
-    silent = true,
-    desc = "paste from system clipboard at cursor"
+	silent = true,
+	desc = "paste from system clipboard at cursor",
 })
 vim.keymap.set("n", "<Leader>pP", [["+P]], {
-    noremap = true,
-    silent = true,
-    desc = "paste from system clipboard before cursor"
+	silent = true,
+	desc = "paste from system clipboard before cursor",
 })
 
 -- Replace selection without yanking
 vim.keymap.set({ "v", "x" }, "R", [["_dP]], {
-    noremap = true,
-    silent = true,
-    desc = "Replace selection with unnamed register"
+	silent = true,
+	desc = "Replace selection with unnamed register",
 })
 vim.keymap.set({ "v", "x" }, "<Leader>R", [["+p]], {
-    noremap = true,
-    silent = true,
-    desc = "Replace selection with system clipboard"
+	silent = true,
+	desc = "Replace selection with system clipboard",
 })
 
-
 -- =============================================================================
--- BUFFER NAVIGATION 
+-- BUFFER NAVIGATION
 -- =============================================================================
 vim.keymap.set("n", "gn", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "gp", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
-
 
 -- =============================================================================
 -- WINDOW MANAGEMENT
@@ -124,14 +107,14 @@ vim.keymap.set("n", "gp", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 
 -- Window splits
 vim.keymap.set("n", "<Leader>wv", "<cmd>vsplit<CR>", {
-    noremap = true,
-    silent = true,
-    desc = "Split Window vertically"
+	noremap = true,
+	silent = true,
+	desc = "Split Window vertically",
 })
 vim.keymap.set("n", "<Leader>ws", "<cmd>split<CR>", {
-    noremap = true,
-    silent = true,
-    desc = "Split Window horizontally"
+	noremap = true,
+	silent = true,
+	desc = "Split Window horizontally",
 })
 
 -- Window navigation
@@ -145,35 +128,50 @@ vim.keymap.set("n", "<Leader>wq", "<cmd>close<CR>", { noremap = true, silent = t
 
 -- Scratch buffers
 vim.keymap.set("n", "<leader>wnv", function()
-    vim.cmd("vnew")
-    vim.bo.buftype = "nofile"
-    vim.bo.bufhidden = "hide"
-    vim.bo.swapfile = false
+	vim.cmd("vnew")
+	vim.bo.buftype = "nofile"
+	vim.bo.bufhidden = "hide"
+	vim.bo.swapfile = false
 end, { desc = "New scratch buffer (vertical)" })
 
 vim.keymap.set("n", "<leader>wns", function()
-    vim.cmd("new")
-    vim.bo.buftype = "nofile"
-    vim.bo.bufhidden = "hide"
-    vim.bo.swapfile = false
+	vim.cmd("new")
+	vim.bo.buftype = "nofile"
+	vim.bo.bufhidden = "hide"
+	vim.bo.swapfile = false
 end, { desc = "New scratch buffer (horizontal)" })
-
 
 -- =============================================================================
 -- TAB MANAGEMENT
 -- =============================================================================
 
 -- Tab creation
-vim.keymap.set({ "n", "t" }, "<Leader>ta", "<cmd>tabnew<CR>",
-    { noremap = true, silent = true, desc = "create a new tab" })
-vim.keymap.set({ "n", "t" }, "<Leader>tb", "<cmd>tabedit %<CR>",
-    { noremap = true, silent = true, desc = "open current buffer in new tab" })
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>ta",
+	"<cmd>tabnew<CR>",
+	{ noremap = true, silent = true, desc = "create a new tab" }
+)
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>tb",
+	"<cmd>tabedit %<CR>",
+	{ noremap = true, silent = true, desc = "open current buffer in new tab" }
+)
 
 -- Tab closing
-vim.keymap.set({ "n", "t" }, "<Leader>tcc", "<cmd>tabclose<CR>",
-    { noremap = true, silent = true, desc = "close current tab" })
-vim.keymap.set({ "n", "t" }, "<Leader>tco", "<cmd>tabonly<CR>",
-    { noremap = true, silent = true, desc = "close other tabs" })
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>tcc",
+	"<cmd>tabclose<CR>",
+	{ noremap = true, silent = true, desc = "close current tab" }
+)
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>tco",
+	"<cmd>tabonly<CR>",
+	{ noremap = true, silent = true, desc = "close other tabs" }
+)
 
 -- Tab navigation
 vim.keymap.set({ "n", "t" }, "tn", "gt", { noremap = true, silent = true, desc = "next tab" })
@@ -183,51 +181,55 @@ vim.keymap.set({ "n", "t" }, "<Leader>t2", "2gt", { noremap = true, silent = tru
 vim.keymap.set({ "n", "t" }, "<Leader>t3", "3gt", { noremap = true, silent = true, desc = "goto tab 3" })
 
 -- Tab reordering
-vim.keymap.set({ "n", "t" }, "<Leader>tm>", "<cmd>tabmove +1<CR>",
-    { noremap = true, silent = true, desc = "move current tab to the right" })
-vim.keymap.set({ "n", "t" }, "<Leader>tm<", "<cmd>tabmove -1<CR>",
-    { noremap = true, silent = true, desc = "move current tab to the left" })
-
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>tm>",
+	"<cmd>tabmove +1<CR>",
+	{ noremap = true, silent = true, desc = "move current tab to the right" }
+)
+vim.keymap.set(
+	{ "n", "t" },
+	"<Leader>tm<",
+	"<cmd>tabmove -1<CR>",
+	{ noremap = true, silent = true, desc = "move current tab to the left" }
+)
 
 -- =============================================================================
 -- TERMINAL MODE
 -- =============================================================================
 
 -- Exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Terminal window navigation
-vim.keymap.set('t', '<A-h>', '<C-\\><C-n><C-w>h', { desc = 'Exit terminal to left split' })
-vim.keymap.set('t', '<A-j>', '<C-\\><C-n><C-w>j', { desc = 'Exit terminal to lower split' })
-vim.keymap.set('t', '<A-k>', '<C-\\><C-n><C-w>k', { desc = 'Exit terminal to above split' })
-vim.keymap.set('t', '<A-l>', '<C-\\><C-n><C-w>l', { desc = 'Exit terminal to right mode' })
-
+vim.keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h", { desc = "Exit terminal to left split" })
+vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j", { desc = "Exit terminal to lower split" })
+vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k", { desc = "Exit terminal to above split" })
+vim.keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l", { desc = "Exit terminal to right mode" })
 
 -- =============================================================================
 -- QUICKFIX LIST
 -- =============================================================================
 
 -- Quickfix navigation
-vim.keymap.set('n', '<leader>cn', '<cmd>cnext<CR>', { desc = 'Next quickfix item' })
-vim.keymap.set('n', '<leader>cp', '<cmd>cprev<CR>', { desc = 'Previous quickfix item' })
-vim.keymap.set('n', '<leader>cf', '<cmd>cfirst<CR>', { desc = 'First quickfix item' })
-vim.keymap.set('n', '<leader>cl', '<cmd>clast<CR>', { desc = 'Last quickfix item' })
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<leader>cf", "<cmd>cfirst<CR>", { desc = "First quickfix item" })
+vim.keymap.set("n", "<leader>cl", "<cmd>clast<CR>", { desc = "Last quickfix item" })
 
 -- Quickfix history
-vim.keymap.set('n', '<leader>co', '<cmd>colder<CR>', { desc = 'Older quickfix list' })
-vim.keymap.set('n', '<leader>ce', '<cmd>cnewer<CR>', { desc = 'Newer quickfix list' })
+vim.keymap.set("n", "<leader>co", "<cmd>colder<CR>", { desc = "Older quickfix list" })
+vim.keymap.set("n", "<leader>ce", "<cmd>cnewer<CR>", { desc = "Newer quickfix list" })
 
 -- Quickfix window
-vim.keymap.set('n', '<leader>qo', '<cmd>copen<CR>', { desc = 'Open quickfix list' })
-vim.keymap.set('n', '<leader>qc', '<cmd>cclose<CR>', { desc = 'Close quickfix list' })
-
+vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>", { desc = "Open quickfix list" })
+vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "Close quickfix list" })
 
 -- =============================================================================
 -- DIAGNOSTICS
 -- =============================================================================
 
-vim.keymap.set('n', '<leader>qd', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
+vim.keymap.set("n", "<leader>qd", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- =============================================================================
 -- LSP
@@ -241,10 +243,20 @@ vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "LS
 
 -- LSP restart
 vim.keymap.set("n", "<leader>lr", function()
-    vim.lsp.stop_client(vim.lsp.get_clients())
-    vim.cmd("edit")
-end, { desc = "LSP Restart" })
+	local clients = vim.lsp.get_clients()
+	local names = {}
+	for _, client in ipairs(clients) do
+		if not vim.tbl_contains(names, client.name) then
+			table.insert(names, client.name)
+		end
+	end
 
+	for _, name in ipairs(names) do
+		vim.lsp.enable(name, false)
+		vim.lsp.enable(name, true)
+	end
+	vim.notify("LSP restarted: " .. table.concat(names, ", "), vim.log.levels.INFO)
+end, { desc = "LSP Restart" })
 
 -- =============================================================================
 -- FILE & DIRECTORY
@@ -252,9 +264,8 @@ end, { desc = "LSP Restart" })
 
 -- Change directory to current file
 vim.keymap.set("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>', {
-    desc = "Change directory to the current file"
+	desc = "Change directory to the current file",
 })
-
 
 -- =============================================================================
 -- PLUGIN-SPECIFIC
@@ -263,39 +274,37 @@ vim.keymap.set("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))
 -- Neo-tree
 vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", { desc = "Toggle Neo-tree" })
 
-
 -- =============================================================================
 -- RELOAD CONFIG
 -- =============================================================================
 
 vim.keymap.set("n", "<leader>rc", function()
-    vim.cmd("source " .. vim.env.MYVIMRC)
-    vim.notify("Config reloaded!", vim.log.levels.INFO)
+	vim.cmd("source " .. vim.env.MYVIMRC)
+	vim.notify("Config reloaded!", vim.log.levels.INFO)
 end, { desc = "Reload config" })
-
 
 -- =============================================================================
 -- Create / Open a Daily Note Markdown file
 -- =============================================================================
-vim.keymap.set('n', '<leader>nn', function()
-    -- Get today's date in Obsidian format (YYYY-MM-DD)
-    local date = os.date("%Y-%m-%d")
+vim.keymap.set("n", "<leader>nn", function()
+	-- Get today's date in Obsidian format (YYYY-MM-DD)
+	local date = os.date("%Y-%m-%d")
 
-    -- Detect OS and set path accordingly
-    local dir
-    if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-        -- Windows OneDrive path
-        dir = "C:/Users/awodzins/OneDrive - Milwaukee Tool/Documents/Notes/Daily notes"
-    else
-        -- Linux/Mac path
-        dir = vim.fn.expand("~/Notes/DailyNotes")
-    end
+	-- Detect OS and set path accordingly
+	local dir
+	if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+		-- Windows OneDrive path
+		dir = "C:/Users/awodzins/OneDrive - Milwaukee Tool/Documents/Notes/Daily notes"
+	else
+		-- Linux/Mac path
+		dir = vim.fn.expand("~/Notes/DailyNotes")
+	end
 
-    local filepath = dir .. "/" .. date .. ".md"
+	local filepath = dir .. "/" .. date .. ".md"
 
-    -- Create directory if it doesn't exist
-    vim.fn.mkdir(dir, "p")
+	-- Create directory if it doesn't exist
+	vim.fn.mkdir(dir, "p")
 
-    -- Open the file (creates it if it doesn't exist)
-    vim.cmd("edit " .. filepath)
+	-- Open the file (creates it if it doesn't exist)
+	vim.cmd("edit " .. filepath)
 end, { desc = "Open today's daily note" })
--- lua/Adan/plugins/org/org_bullets.lua
+++ lua/Adan/plugins/org/org_bullets.lua
@@ -1,19 +1,20 @@
 return {
-  'nvim-orgmode/org-bullets.nvim',
-  ft = { 'org' },
-  opts = {
-    concealcursor = false, -- Show bullets when cursor is on that line
-    symbols = {
-      headlines = { "◉", "○", "✸", "✿" },
-      -- Alternative styles you can try:
-      -- headlines = { "⁖", "◉", "○", "✸", "✿" },
-      -- headlines = { "I", "II", "III", "IV" },
-      -- headlines = { "1.", "2.", "3.", "4." },
-      checkboxes = {
-        half = { "[-]", "@org.checkbox.halfchecked" },
-        done = { "[X]", "@org.checkbox.checked" },
-        todo = { "[ ]", "@org.checkbox" },
-      },
-    },
-  },
+	"nvim-orgmode/org-bullets.nvim",
+	ft = { "org" },
+	opts = {
+		concealcursor = false, -- Show bullets when cursor is on that line
+		symbols = {
+			headlines = { "◉", "○", "✸", "✿" },
+			-- Alternative styles you can try:
+			-- headlines = { "⁖", "◉", "○", "✸", "✿" },
+			-- headlines = { "I", "II", "III", "IV" },
+			-- headlines = { "1.", "2.", "3.", "4." },
+			list = "•",
+			checkboxes = {
+				half = { "◔", "@org.checkbox.halfchecked" },
+				done = { "✓", "@org.keyword.done" },
+				todo = { "˟", "@org.keyword.todo" },
+			},
+		},
+	},
 }
