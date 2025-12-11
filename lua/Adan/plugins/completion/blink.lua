f2b2c993cb5bf912cd277a35351c238454509672
AdanWodzinski <adan.wodzinski@milwaukeetool.com>
2025-12-10 22:10:44 -0600

    change acceptance to meta enter to avoid issue where i insert a new line

--- lua/Adan/plugins/completion/blink.lua
+++ lua/Adan/plugins/completion/blink.lua
@@ -8,7 +8,7 @@ return {
     opts = {
         keymap = {
             -- preset = "enter",
-            ["<CR>"] = { "accept", "fallback" },     -- use enter to accept
+            ["<M-CR>"] = { "accept", "fallback" },     -- use enter to accept
             ['<Tab>'] = { 'select_next', 'fallback' }, --use tab to select next option
             ['<S-Tab>'] = { 'select_prev', 'fallback' }, --use shift tab to go back an option
             ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
