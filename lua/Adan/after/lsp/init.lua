9b0cc01b898d038e6b46abbcbe0f715a55bb670a
adan <adan@adans-MacBook-Air.local>
2026-03-14 22:29:21 -0500

    add hover plugin

--- lua/Adan/after/lsp/init.lua
+++ lua/Adan/after/lsp/init.lua
@@ -1,19 +1,12 @@
 ---@type vim.lsp.Config
-local config = {
-  ---@type lspconfig.settings.lua_ls
+local pyrefly_config = {
   settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      workspace = {
-        preloadFileSize = 10000,
-        library = {
-          vim.env.VIMRUNTIME,
-        }
+    python = {
+      pyrefly = {
+        displayTypeErrors = 'force-on',
       },
     },
   },
 }
 
-vim.lsp.config('lua_ls', config)
+vim.lsp.config('pyrefly', pyrefly_config)
